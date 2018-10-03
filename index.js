f81243cfa114c339adeacaf16c53c5b862b75219
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-10-03 17:56:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function dwarfRollCall(dwarves) {
+  finalString = ''
+  for (i = 0; i < dwarves.length+1; i++){
+    finalString += (dwarves.length + 1).toString();
+    finalString += '. ';
+    finalString += dwarves[i];
+    finalString += ' ';
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
